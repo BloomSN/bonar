5de750b11ec2fc68ed9bf6abed5df1404dd193ca
Nejc Landeker <nejc@bloom.si>
2016-04-02 19:08:10 +0200

    Updating Features api

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,19 +1,19 @@
 class FeaturesController < ApplicationController
 	
   def features_ids
-    if stale?(Features.active.first)
-      render json: Features.active, scope: :ids, root: false
+    if stale?(Feature.active.first)
+      render json: Feature.active, scope: :ids, root: false
     end
   end
   
   def features_detailed
-    if stale?(Features.active.first)
-      render json: Features.active, scope: :detailed, root: false
+    if stale?(Feature.active.first)
+      render json: Feature.active, scope: :detailed, root: false
     end
   end  
   
   def feature_by_id
-    @restaurant = Features.find(params[:id])
-    render json: Features.active.where('id = ?', @restaurant.id), scope: :name, root: false
+    @feature = Feature.find(params[:id])
+    render json: Feature.active.where('id = ?', @feature.id), scope: :name, root: false
   end
 end
