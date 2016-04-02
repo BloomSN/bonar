6bf74f22b56ad6c29d5c2818499fb7d728b7ceb4
Nejc Landeker <nejc@bloom.si>
2016-04-02 20:11:45 +0200

    Features problem fixing

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,19 +1,19 @@
 class FeaturesController < ApplicationController
 	
   def ids
-  	if stale?(Feature.first)
+  	if stale?(Feature)
       render json: Feature, scope: :ids, root: false
     end
   end
   
   def features_detailed
-  	if stale?(Feature.first)
+  	if stale?(Feature)
       render json: Feature, scope: :detailed, root: false
     end
   end  
   
   def find_by_id
     @feature = Feature.find(params[:id])
-    render json: Feature.active.where('id = ?', @feature.id), scope: :name, root: false
+    render json: Feature.where('id = ?', @feature.id), scope: :name, root: false
   end
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,6 +2,10 @@ class Feature < ActiveRecord::Base
   def restaurants
     Restaurant.where('features_array @> ARRAY[?]', id)
   end
+  
+  def features
+    Feature.where(id: features_array).order(:title)
+  end
 
   def html_title
     title.parameterize
